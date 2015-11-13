c6277c6e0547b1c0d6d9088cc58aabc30f5d614e
svnathan <svnathan@stanford.edu>
2015-11-13 14:31:23 -0800

    Random 2

--- recommend.py
+++ recommend.py
@@ -21,50 +21,50 @@ with open(directory + 'Eigen_Value_' + graph + '_' + item +'.txt', 'r') as infil
 
 # Read in nodesAtHop
 inFiles = [f for f in listdir(inputDirectory) 
-        if isfile(join(inputDirectory,f)) ]
-
+	if isfile(join(inputDirectory,f)) ]
+inFiles = ['cluster_123']
 
 nodesAtHop = []
 for filename in inFiles:
-        
-with open(directory + 'NodeAtHop_' + graph + '_' + item + '.txt', 'r') as infile3:
-	nodesAtHop = json.load(infile3)
+	with open(inputDirectory + filename, 'r') as infile3:
+		curCluster = json.load(infile3)
+	nodesAtHop.append(curCluster)
 
-filename = directory + '/Clusters_Users/Edge_List_' + graph + '_' + item + '.tree'
+# filename = directory + '/Clusters_Users/Edge_List_' + graph + '_' + item + '.tree'
 
+# # Read in communities 
+# communitiesToNode = {}
+# nodeToCommunities = {}
+# N = 400
+# with open(filename, 'r') as inputFile:
+# 	idx = 1
+# 	NIdV = snap.TIntV()
+# 	for line in inputFile.readlines():
+# 		if line[0] == '#':
+# 			continue
+# 		if idx > N:
+# 			continue
+# 		colon = line.split(':',1)
+# 		clusterNum = int(colon[0])
+# 		nodeId = int(colon[1].split()[-1])
+# 		if not nodeId in nodeToCommunities: 
+# 			nodeToCommunities[nodeId] = set()
+# 		nodeToCommunities[nodeId].add(clusterNum)
 
-# Read in communities 
-communitiesToNode = {}
-nodeToCommunities = {}
-N = 400
-with open(filename, 'r') as inputFile:
-        idx = 1
-        NIdV = snap.TIntV()
-        for line in inputFile.readlines():
-                if line[0] == '#':
-                        continue
-                if idx > N:
-                        continue
-                colon = line.split(':',1)
-                clusterNum = int(colon[0])
-                nodeId = int(colon[1].split()[-1])
-                if not nodeId in nodeToCommunities: 
-                        nodeToCommunities[nodeId] = set()
-                nodeToCommunities[nodeId].add(clusterNum)
-
-                if not clusterNum in communitiesToNode:
-                        communitiesToNode[clusterNum] = set() 
-                communitiesToNode[clusterNum].add(nodeId)
+# 		if not clusterNum in communitiesToNode:
+# 			communitiesToNode[clusterNum] = set() 
+# 		communitiesToNode[clusterNum].add(nodeId)
 
 userToItems = {}
 with open(directory + '_User_Item_' + item + '.txt', 'r') as infile4:
-        userToItems = json.load(infile4)
+	userToItems = json.load(infile4)
 
 
-usersGraph = snap.LoadEdgeList(snap.PUNGraph, directory + 'Edge_List_' + graph + '_' + item +'.txt', 0, 1, '\t')
+usersGraph = snap.LoadEdgeList(snap.PUNGraph, directory + 'Edge_List_Users_' + item +'.txt', 0, 1, '\t')
 itemsGraph = snap.LoadEdgeList(snap.PUNGraph, directory + 'Edge_List_Items_' + item +'.txt', 0, 1, '\t')
 
 # Recommend 
+
 
 
 # Compare against ground truth 
